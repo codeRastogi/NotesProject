a05fc91b22dedf68eb0caf564081cbd5b8cd5921
codeRastogi <harshit1122ras@gmail.com>
2026-02-13 01:24:05 +0530

    done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import NavBar from '../components/NavBar'
+import RateLImitedUI from '../components/RateLImitedUI'
+import api from '../lib/axios'
+import toast from 'react-hot-toast'
+import NoteCard from '../components/NoteCard'
+import NoNotesFound from '../components/NoNotesFound'
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await api.get("/notes")
+        setNotes(res.data)
+        setIsRateLimited(false)
+        console.log(res.data)
+      } catch (error) {
+        console.log("AXIOS ERROR:", error);
+
+        if (error.response && error.response.status === 429) {
+          setIsRateLimited(true);
+          return;
+        }
+
+        toast.error("Error fetching notes");
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchNotes()
+  }, [])
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <NavBar />
+
+      {isRateLimited && <RateLImitedUI />}
+
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'><span className="loading loading-bars loading-xl"></span></div>}
+        {notes.length === 0 && !isRateLimited && <NoNotesFound />}
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,127 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router';
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
+import { ArrowLeftIcon, Trash2Icon } from 'lucide-react'
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.error("Error fetching note:", error);
+        toast.error("Error fetching note");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchNote();
+  }, [id])
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`)
+      toast.success("Note deleted successfully");
+      navigate("/");
+    } catch (error) {
+      toast.error("Error deleting note");
+    }
+  }
+  const handleSave = async () => {
+    if (!note.title.trim() || !note.content.trim()) {
+      toast.error("Title and content cannot be empty");
+      return;
+    }
+    setSaving(true);
+    try {
+      await api.put(`/notes/${id}`, { title: note.title, content: note.content })
+      toast.success("Note updated successfully");
+      navigate("/");
+    } catch (error) {
+      toast.error("Error updating note");
+    }
+    finally {
+      setSaving(false);
+    }
+  }
+
+
+  if (loading) {
+    return <div className='h-screen text-center text-primary py-10'><span className="loading loading-bars loading-xl"></span></div>
+  }
+
+
   return (
-    <div>NoteDetailPage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className="max-w-2xl mx-auto">
+
+          <div className='flex items-center justify-between mb-6'>
+            <Link
+              to="/"
+              className='btn btn-ghost'>
+              <ArrowLeftIcon className='w-5 h-5' />
+              Back to Notes
+            </Link>
+            <button onClick={handleDelete} className='btn btn-error btn-outline'>
+              <Trash2Icon className='w-5 h-5' />
+              Delete Note
+            </button>
+          </div>
+
+          <div className='card bg-base-100'>
+            <div className='card-body'>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className='label-text'>Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder='Note title'
+                  className='input input-bordered'
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+
+
+
+              </div>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className='label-text'>Content</span>
+                </label>
+                <textarea
+                  placeholder='Note content'
+                  className='textarea textarea-bordered h-32'
+                  value={note.content}
+                  onChange={(e) => setNote({ ...note, content: e.target.value })}
+                />
+              </div>
+
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary"
+                  disabled={saving}
+                  onClick={handleSave}>
+                  {saving ? <span className="loading loading-spinner"></span> : "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
